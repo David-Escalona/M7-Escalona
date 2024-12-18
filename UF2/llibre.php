87d38d36d4241d94f1b96d6a284d5041d24b494a
David-Escalona <120495685+David-Escalona@users.noreply.github.com>
2024-12-18 15:30:45 +0000

    Ejercicio 3

--- UF2/llibre.php
+++ UF2/llibre.php
@@ -5,8 +5,16 @@
         public string $titol = "Don Xicote de la Mancha";
         public string $autor = "Miguelito";
 
+        public function __construct(string $titol, int $autor) {
+            $this->titol = $titol;
+            $this->autor = $autor;
+            }
+
         public function descripcio(): string {
 
     return "El libro " . $this->titol . "a sido escrito por: " . $this->autor . ".";
     }
 }
+
+$llibre = new Llibre("Don Xicote de la Mancha", "Miguelito");
+echo $Llibre->descriure();
