b1722db4eec11397884af31e2eee8eda0b959df0
David-Escalona <120495685+David-Escalona@users.noreply.github.com>
2024-12-18 16:12:33 +0000

    Ejercicio 7

--- UF2/Actividad-1.php
+++ UF2/Actividad-1.php
@@ -1,21 +1,21 @@
 <?php
 
-class Llibre {
+    class Llibre {
 
-    public string $titol;
-    public string $autor;
+        public string $titol;
+        public string $autor;
 
-    public function __construct(string $titol = "Don Xicote de la Mancha", string $autor = "Miguelito") {
-        $this->titol = $titol;
-        $this->autor = $autor;
-    }
+        public function __construct(string $titol = "Don Xicote de la Mancha", string $autor = "Miguelito") {
+            $this->titol = $titol;
+            $this->autor = $autor;
+        }
 
-    public function descripcio(): string {
-        return "El libro " . $this->titol . " ha sido escrito por: " . $this->autor . ".";
-    }
+        public function descripcio(): string {
+            return "El libro " . $this->titol . " ha sido escrito por: " . $this->autor . ".";
+        }
 
-    public function getAutor() {
-        echo "El autor es " . $this->autor;
+        public function getAutor() {
+            echo "El autor es " . $this->autor;
     }
 }
 
@@ -23,19 +23,35 @@ $llibre = new Llibre("Don Xicote de la Mancha", "Miguelito");
 echo $llibre->descripcio();
 
 
-class Persona {
-    public string $nom;
-    public int $edad;
+    class Persona {
+        public string $nom;
+        public int $edad;
 
-    public function __construct(string $nom = "Anna", int $edad = 25) {
-        $this->nom = $nom;
-        $this->edad = $edad;
-    }
+        public function __construct(string $nom = "Anna", int $edad = 25) {
+            $this->nom = $nom;
+            $this->edad = $edad;
+        }
 
-    public function saludar(): string {
-        return "Hola, soy " . $this->nom . " y tengo " . $this->edad . " años.";
+        public function saludar(): string {
+            return "Hola, soy " . $this->nom . " y tengo " . $this->edad . " años.";
     }
 }
+
+    class Producte {
+        public string $nom;
+        public int $preu;
+
+        public function __construct(string $nom = "David", int $preu = 2000000000) {
+            $this->nom = $nom;
+            $this->preu = $preu;
+        }
+
+        public function mostrarPreu(): string {
+            return "Hola, soy " . $this->nom . " y mi casa vale " . $this->preu . " euros.";
+    }
+}
+
+
 ?>
 
 <!DOCTYPE html>
