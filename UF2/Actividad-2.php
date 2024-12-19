d092612b1c428b36a494e462770fee268c7cedf9
David-Escalona <120495685+David-Escalona@users.noreply.github.com>
2024-12-19 09:05:45 +0000

    Actividad 2 - Ejercicio 1 (ACTUALIZADO)

--- UF2/Actividad-2.php
+++ UF2/Actividad-2.php
@@ -1,8 +1,8 @@
 <?php
     class Cotxe {
         
-        public string $marca = "SEAT";
-        public string $model = "ARONA";
+        public string $marca;
+        public string $model;
         
         public function descripcio(): string {
         return "Aquest cotxe és un " . $this->marca . " " . $this->model . ".";
