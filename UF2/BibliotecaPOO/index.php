190dfb18b675831bfa4c5bfe062035d71630da8a
David-Escalona <120495685+David-Escalona@users.noreply.github.com>
2025-01-09 11:50:42 +0000

    Guardar libros completado

--- UF2/BibliotecaPOO/index.php
+++ UF2/BibliotecaPOO/index.php
@@ -1,6 +1,6 @@
 <?php
 
-//Ejecutar codigo con cd ruta/del/archivo
+//Ejecutar codigo con cd UF2/BibliotecaPOO
 //php -S 0.0.0.0:8000
 
 session_start();
@@ -17,28 +17,25 @@ class Libro {
         $this->autor = $autor;
         $this->publicacion = $publicacion;
         $this->foto = $foto;
-    }
-
-    public function obtenirDetalls() {
-        return "$this->titulo - $this->autor ($this->publicacion)";
-    }
-
-}
-
-
-class Biblioteca {
-
-    public $libros = [];
-
-    public function añadirLibro($libro) {
-        $this->libros[] = $libro;
-    }
+    } 
 
     public function mostrarLibro() {
-        return $this->libros;
+        return "<h3><strong>Titulo:</strong> $this->titulo</h3>
+                <h3><strong>Autor:</strong> $this->autor</h3>
+                <h3><strong>Año de publicación:</strong> $this->publicacion</h3>
+                <img src='$this->foto' alt='Imagen del libro' width='500'>";
     }
 
 }
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $titulo = $_POST['titulo'];
+    $autor = $_POST['autor'];
+    $publicacion = $_POST['publicacion'];
+    $foto = $_POST['foto'];
+}
+
+$libro = new Libro($titulo, $autor, $publicacion, $foto);
 
 ?>
 
@@ -48,7 +45,30 @@ class Biblioteca {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Bungee+Spice&display=swap" rel="stylesheet">
     <title>Biblioteca POO</title>
+    <style>
+
+        html, h5, label{
+            font-family: Bungee Spice;
+        }
+
+        h2{
+            margin-top: 20px;
+            margin-bottom: 20px;
+            display: flex;
+            justify-content: center;
+            font-family: Bungee Spice;
+        }
+
+        h3{
+            color: black;
+            font-family: Bungee Spice;
+        }
+
+    </style>
 </head>
 <body class="bg-light">
 
@@ -56,7 +76,7 @@ class Biblioteca {
         <h1 class="text-center mb-4">Gestió de Biblioteca</h1>
     </div>
 
-    <div class="card mb-2 bg-secondary border border-dark rounded">
+    <div class="card mb-2 bg-dark border border-dark rounded container">
         <div class="card-body text-light">
             <h5 class="card-title text-center">Añade un libro</h5>
             <form method="POST">
@@ -79,6 +99,21 @@ class Biblioteca {
                 <button type="submit" name="afegir" class="btn btn-primary border border-dark rounded-circle">Añadir libro</button>
             </form>
         </div>
+    </div>
+
+    <div class="card container bg-dark text-center">
+
+<?php
+
+if (isset($libro)) {
+    echo "<h2>Libro Añadido</h2>";
+    echo "<div>";
+    echo $libro->mostrarLibro();
+    echo "</div>";
+}
+
+?>
+    
     </div>
 
 </body>
