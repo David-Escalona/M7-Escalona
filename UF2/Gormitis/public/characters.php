dafc35c881ca10934f36d8280dad5d2baac659d5
David-Escalona <120495685+David-Escalona@users.noreply.github.com>
2025-01-14 11:23:40 +0000

    Commit

--- UF2/Gormitis/public/characters.php
+++ UF2/Gormitis/public/characters.php
@@ -2,54 +2,46 @@
 
 include '../src/config/config.php';
 
-$personatges = isset($_SESSION['gormitis']) ? $_SESSION['gormitis'] : [];
-
 ?>
 
 <!DOCTYPE html>
-<html lang="ca">
+<html lang="es">
 <head>
-    <meta charset="UTF-8">
+<meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Selecció de personatges</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <link href="https://fonts.googleapis.com/css2?family=Bungee+Spice&display=swap" rel="stylesheet">
+    <title>Selecció de Personatges</title>
+    <style>
+        body{
+            background-color: black;
+        }
+
+        .letratitulo{
+            font-family: Bungee Spice;
+        }
+        .color{
+            background-color:rgb(213, 213, 213);
+        }
+
+    </style>
 </head>
 <body>
-    <h1>Selecciona els teus personatges per al combat</h1>
+    <h1 class="text-center m-5 letratitulo">Personatges Creats</h1>
 
-    <?php if (empty($personatges)): ?>
-        <p>No hi ha personatges creats encara.</p>
-        <p><a href="index.php">Crea un nou Gormiti</a></p>
-    <?php else: ?>
-        <form action="precombat.php" method="post">
-            <div style="display: flex; flex-wrap: wrap;">
-                <?php foreach ($personatges as $index => $serialitzat): 
-                   
-                    $gormiti = unserialize($serialitzat); 
-                ?>
-                    <div style="border: 1px solid #ccc; padding: 10px; margin: 10px; width: 200px; text-align: center;">
-                        <img src="<?php echo htmlspecialchars($gormiti->imatge); ?>" alt="<?php echo htmlspecialchars($gormiti->nom); ?>" style="width: 100%; height: auto;">
-                        <h3><?php echo htmlspecialchars($gormiti->nom); ?></h3>
-                        <p>Salut: <?php echo htmlspecialchars($gormiti->salut); ?></p>
-                        <p>Dany: <?php echo htmlspecialchars($gormiti->dany); ?></p>
-                        <p>Habilitats:</p>
-                        <ul>
-                            <?php foreach ($gormiti->habilitats as $habilitat): ?>
-                                <li><?php echo htmlspecialchars($habilitat); ?></li>
-                            <?php endforeach; ?>
-                        </ul>
-                        <label>
-                            <input type="checkbox" name="seleccionats[]" value="<?php echo $index; ?>">
-                            Selecciona
-                        </label>
-                    </div>
-                <?php endforeach; ?>
-            </div>
+    <div class="d-flex d-row">
+        <div class="card container">
 
-            <button type="submit">Enviar personatges seleccionats</button>
-        </form>
-    <?php endif; ?>
+        <?php
+        
+        ?>
 
-    <p><a href="index.php">Torna a crear més Gormitis</a></p>
-    <p><a href="reset.php">Reinicia la sessió</a></p>
+        </div>
+    </div>
+    
+    <div class="card container mt-5 d-flex flex-row justify-content-center mb-5">
+        <p class="m-5 letratitulo shadow p-3 border color w-100 d-flex justify-content-center fs-4"><a href="index.php" class="text-decoration-none">Torna a crear més Personatges</a></p>
+        <p class="m-5 letratitulo shadow p-3 border color w-100 d-flex justify-content-center fs-4"><a href="reset.php" class="text-decoration-none">Reiniciar la sessió</a></p>
+    </div>
 </body>
 </html>
--- UF2/Gormitis/public/index.php
+++ UF2/Gormitis/public/index.php
@@ -12,7 +12,7 @@ include '../src/classes/Gormiti.php';
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <link href="https://fonts.googleapis.com/css2?family=Bungee+Spice&display=swap" rel="stylesheet">
-    <title>Crear Gormitis</title>
+    <title>Crear Personatge</title>
     <style>
         body{
             background-color: black;
@@ -21,41 +21,44 @@ include '../src/classes/Gormiti.php';
         .letratitulo{
             font-family: Bungee Spice;
         }
+        .color{
+            background-color:rgb(213, 213, 213);
+        }
+
     </style>
 </head>
 <body>
     
-<h1 class="text-center m-5 letratitulo">Crear un nou</h1>
-    <?php if (isset($error)): ?>
-        <p style="color: red;"><?php echo $error; ?></p>
-    <?php endif; ?>
+<h1 class="text-center m-5 letratitulo">Crear un nou Personatge</h1>
 
     <div class="container card border p-5">
         <form action="index.php" method="post">
-            <label for="nom" class="letratitulo">Nom:</label><br>
-            <input type="text" name="nom" id="nom" required><br><br>
+            <label for="nom" class="letratitulo ">Nom:</label><br>
+            <input type="text" class="shadow border" name="nom" id="nom" required><br><br>
 
             <label for="salut" class="letratitulo">Salut:</label><br>
-            <input type="number" name="salut" id="salut" required><br><br>
+            <input type="number" class="shadow border" name="salut" id="salut" required><br><br>
 
             <label for="dany" class="letratitulo">Dany:</label><br>
-            <input type="number" name="dany" id="dany" required><br><br>
+            <input type="number" class="shadow border" name="dany" id="dany" required><br><br>
 
             <label for="imatge" class="letratitulo">URL de la imatge:</label><br>
-            <input type="text" name="imatge" id="imatge" required><br><br>
+            <input type="text" class="shadow border" name="imatge" id="imatge" required><br><br>
 
             <label for="habilitats" class="letratitulo">Habilitats (separades per comes):</label><br>
-            <input type="text" name="habilitats" id="habilitats" required><br><br>
+            <input type="text" class="shadow border" name="habilitats" id="habilitats" required><br><br>
 
-            <button type="submit" class="letratitulo">Crear Gormiti</button>
+            <div class="d-flex justify-content-center mt-4">
+                <button type="submit" class="letratitulo shadow border p-3 color w-100 d-flex justify-content-center fs-4">Crear Personatge</button>
+            </div>
+            
         </form>
     </div>
     
     <div class="card container mt-5 d-flex flex-row justify-content-center mb-5">
-        <p class="m-5 letratitulo shadow p-3 border"><a href="characters.php" class="text-decoration-none">Veure Gormitis</a></p>
-        <p class="m-5 letratitulo shadow p-3 border"><a href="reset.php" class="text-decoration-none">Reiniciar la sessió</a></p>
+        <p class="m-5 letratitulo shadow p-3 border color w-100 d-flex justify-content-center fs-4"><a href="characters.php" class="text-decoration-none">Veure els Personatges</a></p>
+        <p class="m-5 letratitulo shadow p-3 border color w-100 d-flex justify-content-center fs-4"><a href="reset.php" class="text-decoration-none">Reiniciar la sessió</a></p>
     </div>
     
 </body>
 </html>
-
