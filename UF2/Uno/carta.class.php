936b22ece5a7b1298d6a44facae7b014f6ae9af0
David-Escalona <120495685+David-Escalona@users.noreply.github.com>
2025-01-22 11:06:21 +0000

    Commit

--- UF2/Uno/carta.class.php
+++ UF2/Uno/carta.class.php
@@ -1,32 +1,23 @@
 <?php
 
-    class Carta {
+class Carta {
 
-        public string $palo;
-        public int $numero;
-        public int $index;
+    public string $palo;
+    public int $numero;
+    public int $index;
 
-        public function __construct($palo, $numero, $index){
+    public function __construct($palo, $numero, $index) {
+        $this->palo = $palo;
+        $this->numero = $numero;
+        $this->index = $index;
+    }
 
-            $this->palo = $palo;
-            $this->numero = $numero;
-            $this->index = $index;
+    public function pinta_carta() {
+        return "<img src='img/{$this->palo}_{$this->numero}.png' alt='{$this->palo} {$this->numero}' />";
+    }
 
-        }
-
-        public function pinta_carta(){
-
-            return "<img src='cartas_uno/{$this->palo}_{$this->numero}.png' alt='{$this->palo} {$this->numero}' />";
-
-        }
-
-        public function pinta_carta_link(){
-
-        }
-
-        public function pinta_carta_girada(){
-
-        }
+    public function pinta_carta_girada() {
+        return "<img src='img/carta_girada.png' alt='Carta Girada' />";
+    }
 }
-
 ?>
