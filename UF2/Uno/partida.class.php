e5765f33214ce6964f24d6d61e7e57f84c246a87
David-Escalona <120495685+David-Escalona@users.noreply.github.com>
2025-01-23 09:53:42 +0000

    Comentado PHP partida.class

--- UF2/Uno/partida.class.php
+++ UF2/Uno/partida.class.php
@@ -1,47 +1,50 @@
 <?php
 
-    session_start();
-    include 'baraja.class.php';
+    session_start(); // Inicio la sesion de PHP
+    include 'baraja.class.php'; // Incluyo variables del archivo baraja
 
-    $jugadores = isset($_POST['jugadores']) ? (int)$_POST['jugadores'] : 5;
-    $cartasPorJugador = isset($_POST['cartas']) ? (int)$_POST['cartas'] : 5;
+    $jugadores = isset($_POST['jugadores']) ? (int)$_POST['jugadores'] : 5; // Asigno un maximo de 5 jugadores a la partida
+    $cartasPorJugador = isset($_POST['cartas']) ? (int)$_POST['cartas'] : 7; // Asigno un maximo de cartas a la partirda
 
-    $baraja = new Baraja();
+    $baraja = new Baraja(); // Creo una instancia de la clase Baraja
 
-    function repartirCartas($baraja, $jugadores, $cartasPorJugador) {
-        $manos = [];
-        for ($i = 0; $i < $jugadores; $i++) {
-            $mano = [];
-            for ($j = 0; $j < $cartasPorJugador; $j++) {
-                $mano[] = $baraja->obtenerCartaAleatoria();
+    function repartirCartas($baraja, $jugadores, $cartasPorJugador) { // Creo una funcion reparitCartas(con tres variables)
+        $manos = []; // Creo un array vacio
+        for ($i = 0; $i < $jugadores; $i++) { // Hago un bucle de entrada donde la I inicializa en 0
+            // Y debe ser menor al numero de jugadores que son (5 por defecto) 
+            $mano = []; // Si se cumplen los parametros se crea otro array vacio
+            for ($j = 0; $j < $cartasPorJugador; $j++) { // Hago un bucle de salida con el valor j con valor de 0 y menor a las cartas
+                $mano[] = $baraja->obtenerCartaAleatoria(); // Dentro del array vacio se guardaran todos los datos de la classe baraja
+                // Que hemos instanciado antes recogiendo los datos de la funcion obtenerCartaAleatoria de la clase baraja.class.php
             }
-            $manos[] = $mano;
+            $manos[] = $mano; // En el array de manos de guarda la carta aleatorio del array mano
         }
-        return $manos;
+        return $manos; // Devolvemos el array
 }
 
-    $manos = repartirCartas($baraja, $jugadores, $cartasPorJugador);
+    $manos = repartirCartas($baraja, $jugadores, $cartasPorJugador); // En la variable manos se guarda la funcion repartirCartas
 
-    if (!isset($_SESSION['cartaEnMano'])) {
-        $_SESSION['cartaEnMano'] = $baraja->obtenerCartaAleatoria();
+    if (!isset($_SESSION['cartaEnMano'])) { // Si la variable cartaen mano no existe
+        $_SESSION['cartaEnMano'] = $baraja->obtenerCartaAleatoria(); // Se crea una una carta aleatoria
     }
 
-    if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['robar'])) {
-        $jugadorIndex = (int)$_POST['jugadorIndex'];
-        $nuevaCarta = $baraja->obtenerCartaAleatoria();
-        echo json_encode(['jugadorIndex' => $jugadorIndex, 'carta' => $nuevaCarta]);
-        exit;
+    if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['robar'])) { // Si se ha enviado una solicitud para robar
+        $jugadorIndex = (int)$_POST['jugadorIndex']; // Se comprueba que jugador es
+        $nuevaCarta = $baraja->obtenerCartaAleatoria(); // En la variable nuevacarta se robara una nueva
+        echo json_encode(['jugadorIndex' => $jugadorIndex, 'carta' => $nuevaCarta]); // Se le asigna la carta robada al jugador
+        exit; // Se termina
     }
 
-    if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['jugarCarta'])) {
-        $jugadorIndex = (int)$_POST['jugadorIndex'];
-        $cartaSeleccionada = $_POST['carta'];
+    if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['jugarCarta'])) { //Se comprueba si se ha enviado una solicutud para jugar
+        $jugadorIndex = (int)$_POST['jugadorIndex']; // Se comprueba el jugador
+        $cartaSeleccionada = $_POST['carta']; // Se guarda la carta
 
-        list($colorEnMano, $numeroEnMano) = explode(' ', $_SESSION['cartaEnMano']);
-        list($colorCarta, $numeroCarta) = explode(' ', $cartaSeleccionada);
+        list($colorEnMano, $numeroEnMano) = explode(' ', $_SESSION['cartaEnMano']); // Este es el valor almacenado de la cartaenmado
+        list($colorCarta, $numeroCarta) = explode(' ', $cartaSeleccionada); // Este es el valor almacenado de la cartaseleccionada
 
-        if ($colorCarta === $colorEnMano || $numeroCarta === $numeroEnMano) {
-            $_SESSION['cartaEnMano'] = $cartaSeleccionada;
+        if ($colorCarta === $colorEnMano || $numeroCarta === $numeroEnMano) { // Si las variables son iguales es decir
+            // El color de la carta es el que esta en mano
+            $_SESSION['cartaEnMano'] = $cartaSeleccionada; // La carta sera valida para poder jugarla
             unset($manos[$jugadorIndex][array_search($cartaSeleccionada, $manos[$jugadorIndex])]);
 
             echo json_encode(['success' => true, 'nuevaCarta' => $cartaSeleccionada, 'jugadorIndex' => $jugadorIndex]);
