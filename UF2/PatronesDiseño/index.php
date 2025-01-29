615bf04ccbb2db172bb6a9dfc43723f88ba7ad14
David-Escalona <120495685+David-Escalona@users.noreply.github.com>
2025-01-29 16:23:44 +0000

    Commit Diseño de patrones

---
+++ UF2/PatronesDiseño/index.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Bungee+Spice&display=swap" rel="stylesheet">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Comfortaa:wght@300..700&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="estilo.css">
+    <title>David Escalona García</title>
+
+    <style>
+
+        main{
+            font-family: Comfortaa;
+        }
+
+    </style>
+
+</head>
+<body>
+    
+    <?php include 'nav.php'; ?>
+
+    <main>
+
+    <div class="d-flex justify-content-center">
+        <h1 class="d-flex justify-content-center text-light mt-5">Patrones de Diseño</h1> 
+    </div>
+
+    <div class="d-flex justify-content-center">
+        <p class="text-light espaciado d-flex justify-content-center">Los patrones de diseño son unas técnicas para resolver problemas comunes en el desarrollo de software y otros ámbitos referentes al diseño de interacción o interfaces.
+        Un patrón de diseño resulta ser una solución a un problema de diseño. Para que una solución sea considerada un patrón debe poseer ciertas características. Una de ellas es que debe haber comprobado su efectividad resolviendo problemas similares en ocasiones anteriores. Otra es que debe ser reutilizable, lo que significa que es aplicable a diferentes problemas de diseño en distintas circunstancias.</p>
+    </div>
+    
+
+    </main>
+
+</body>
+</html>
---
+++ UF2/PatronesDiseño/nav.php
@@ -0,0 +1,44 @@
+<?php
+
+?>
+
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Bungee+Spice&display=swap" rel="stylesheet">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Comfortaa:wght@300..700&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="estilo.css">
+    <title>David Escalona García</title>
+
+    <style>
+        header{
+            font-family: Comfortaa;
+        }
+    </style>
+
+</head>
+<body>
+    
+    <header class="d-flex flex-column align-items-center text-light bg-danger p-4 fixed-left">
+
+        <img src="img/logo.png" alt="Logo" class="mb-4" width="200">
+        
+        <nav>
+            <ul class="list-unstyled text-star">
+                <li class="mb-3 fs-5"><a href="" class="text-light decoracion">Home</a></li>
+                <li class="mb-3 fs-5"><a href="" class="text-light decoracion">Patrones de Diseño</a></li>
+                <li class="mb-3 fs-5"><a href="" class="text-light decoracion">Refactorización</a></li>
+            </ul>
+        </nav>
+        
+    </header>
+
+</body>
+</html>
