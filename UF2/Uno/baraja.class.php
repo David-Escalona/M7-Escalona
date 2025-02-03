4e82cdf29112fe98e9f5aedc78eefcfa5552af0a
David-Escalona <120495685+David-Escalona@users.noreply.github.com>
2025-02-03 11:59:49 +0000

    Commit

--- UF2/Uno/baraja.class.php
+++ UF2/Uno/baraja.class.php
@@ -4,20 +4,22 @@
 
 class Baraja {
     public $conjunto_cartas = [];
-
+    
     public function crea_baraja()
-    {
-        $index = 0;
+{
+    $index = 0;
 
-        foreach (['red', 'yellow', 'blue', 'green'] as $color) {
-            for ($i = 0; $i <= 9; $i++) {
-                $this->conjunto_cartas[] = new Carta($color, $i, $index++);
-            }
-            foreach (['reverse', 'skip', 'picker'] as $especial) {
-                $this->conjunto_cartas[] = new Carta($color, $especial, $index++);
-            }
+    foreach (['red', 'yellow', 'blue', 'green'] as $color) {
+        for ($i = 1; $i <= 9; $i++) {
+            $this->conjunto_cartas[] = new Carta($color, $i, null); // Agrega un valor para el tercer parámetro
         }
+        // Afegeix cartes especials
+        $this->conjunto_cartas[] = new Carta($color, 'reverse', null);
+        $this->conjunto_cartas[] = new Carta($color, 'skip', null);
+        $this->conjunto_cartas[] = new Carta($color, '+2', null);
     }
+}
+   
 
     public function mezcla()
     {
