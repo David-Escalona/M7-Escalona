5ea14ef4bccb68c28f80e0f4e994a383a7c7538d
David-Escalona <120495685+David-Escalona@users.noreply.github.com>
2025-02-21 09:08:56 +0000

    Commit

---
+++ UF3/ProyectoCRUD/theme/feature.php
@@ -0,0 +1,125 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- mobile responsive meta -->
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+    
+    <!-- theme meta -->
+    <meta name="theme-name" content="agen" />
+    
+    <!-- ** Plugins Needed for the Project ** -->
+    <!-- Bootstrap -->
+    <link rel="stylesheet" href="plugins/bootstrap/bootstrap.min.css">
+    <!-- slick slider -->
+    <link rel="stylesheet" href="plugins/slick/slick.css">
+    <!-- themefy-icon -->
+    <link rel="stylesheet" href="plugins/themify-icons/themify-icons.css">
+    <!-- venobox css -->
+    <link rel="stylesheet" href="plugins/venobox/venobox.css">
+    <!-- card slider -->
+    <link rel="stylesheet" href="plugins/card-slider/css/style.css">
+
+    <!-- Main Stylesheet -->
+    <link href="css/style.css" rel="stylesheet">
+    <title>Document</title>
+</head>
+<body>
+    
+<section class="section bg-secondary position-relative">
+  <div class="bg-image overlay-secondary">
+    <img src="images/feature.jpg" alt="bg-image">
+  </div>
+  <div class="container-fluid">
+    <div class="row">
+      <div class="col-xl-9 mx-auto">
+        <div class="row align-items-center">
+          <div class="col-lg-4 mb-4 mb-lg-0">
+            <img src="images/feature.jpg" alt="feature-image" class="img-fluid">
+          </div>
+          <div class="col-lg-7 offset-lg-1">
+            <div class="row">
+              <div class="col-12">
+                <h2 class="text-white">Se como ayudarte</h2>
+                <div class="section-border ml-0"></div>
+              </div>
+              <div class="col-md-6 mb-4">
+                <div class="media">
+                  <i class="icon text-gradient-primary ti-vector mr-3"></i>
+                  <div class="media-body">
+                    <h4 class="text-white">Resultados Garantizados</h4>
+                    <p class="text-light">Tenemos una gran serie de usuarios con resultados inmediantos garantizados.</p>
+                  </div>
+                </div>
+              </div>
+              <div class="col-md-6 mb-4">
+                <div class="media">
+                  <i class="icon text-gradient-primary ti-layout mr-3"></i>
+                  <div class="media-body">
+                    <h4 class="text-white">Cursos Adaptados</h4>
+                    <p class="text-light">Cursos adapatados a los horarios de cada usuario.</p>
+                  </div>
+                </div>
+              </div>
+              <div class="col-md-6 mb-4">
+                <div class="media">
+                  <i class="icon text-gradient-primary ti-headphone-alt mr-3"></i>
+                  <div class="media-body">
+                    <h4 class="text-white">Reuniones Semanales</h4>
+                    <p class="text-light">Reuniones con grupos de usuarios semanales para analizar resultados.</p>
+                  </div>
+                </div>
+              </div>
+              <div class="col-md-6 mb-4">
+                <div class="media">
+                  <i class="icon text-gradient-primary ti-ruler-pencil mr-3"></i>
+                  <div class="media-body">
+                    <h4 class="text-white">Aprendizaje Constante</h4>
+                    <p class="text-light">Aprendizaje diario adaptado a las necesidades de cada usuario.</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
+
+<!-- jQuery -->
+<script src="plugins/jQuery/jquery.min.js"></script>
+<!-- Bootstrap JS -->
+<script src="plugins/bootstrap/bootstrap.min.js"></script>
+<!-- slick slider -->
+<script src="plugins/slick/slick.min.js"></script>
+<!-- venobox -->
+<script src="plugins/venobox/venobox.min.js"></script>
+<!-- shuffle -->
+<script src="plugins/shuffle/shuffle.min.js"></script>
+<!-- apear js -->
+<script src="plugins/counto/apear.js"></script>
+<!-- counter -->
+<script src="plugins/counto/counTo.js"></script>
+<!-- card slider -->
+<script src="plugins/card-slider/js/card-slider-min.js"></script>
+<!-- google map -->
+<script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCcABaamniA6OL5YvYSpB3pFMNrXwXnLwU&libraries=places"></script>
+<script src="plugins/google-map/gmap.js"></script>
+
+<!-- jQuery -->
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"></script>
+
+<!-- Popper.js (necesario para los tooltips y dropdowns) -->
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
+
+<!-- Bootstrap JS -->
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
+
+<!-- Main Script -->
+<script src="js/script.js"></script>
+
+</body>
+</html>
--- UF3/ProyectoCRUD/theme/portfolio.php
+++ UF3/ProyectoCRUD/theme/portfolio.php
@@ -170,66 +170,7 @@ while ($row = $result->fetch_assoc()) {
 <!-- /service -->
 
 <!-- feature -->
-<section class="section bg-secondary position-relative">
-  <div class="bg-image overlay-secondary">
-    <img src="images/feature.jpg" alt="bg-image">
-  </div>
-  <div class="container-fluid">
-    <div class="row">
-      <div class="col-xl-9 mx-auto">
-        <div class="row align-items-center">
-          <div class="col-lg-4 mb-4 mb-lg-0">
-            <img src="images/feature.jpg" alt="feature-image" class="img-fluid">
-          </div>
-          <div class="col-lg-7 offset-lg-1">
-            <div class="row">
-              <div class="col-12">
-                <h2 class="text-white">Se como ayudarte</h2>
-                <div class="section-border ml-0"></div>
-              </div>
-              <div class="col-md-6 mb-4">
-                <div class="media">
-                  <i class="icon text-gradient-primary ti-vector mr-3"></i>
-                  <div class="media-body">
-                    <h4 class="text-white">Resultados Garantizados</h4>
-                    <p class="text-light">Tenemos una gran serie de usuarios con resultados inmediantos garantizados.</p>
-                  </div>
-                </div>
-              </div>
-              <div class="col-md-6 mb-4">
-                <div class="media">
-                  <i class="icon text-gradient-primary ti-layout mr-3"></i>
-                  <div class="media-body">
-                    <h4 class="text-white">Cursos Adaptados</h4>
-                    <p class="text-light">Cursos adapatados a los horarios de cada usuario.</p>
-                  </div>
-                </div>
-              </div>
-              <div class="col-md-6 mb-4">
-                <div class="media">
-                  <i class="icon text-gradient-primary ti-headphone-alt mr-3"></i>
-                  <div class="media-body">
-                    <h4 class="text-white">Reuniones Semanales</h4>
-                    <p class="text-light">Reuniones con grupos de usuarios semanales para analizar resultados.</p>
-                  </div>
-                </div>
-              </div>
-              <div class="col-md-6 mb-4">
-                <div class="media">
-                  <i class="icon text-gradient-primary ti-ruler-pencil mr-3"></i>
-                  <div class="media-body">
-                    <h4 class="text-white">Aprendizaje Constante</h4>
-                    <p class="text-light">Aprendizaje diario adaptado a las necesidades de cada usuario.</p>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
-  </div>
-</section>
+<?php include 'feature.php'?>
 <!-- /feature -->
 
 <!-- call to action -->
