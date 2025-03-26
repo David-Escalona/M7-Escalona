9c53814e5dc8dcbca1048b0278fa78eb82de5558
David-Escalona <120495685+David-Escalona@users.noreply.github.com>
2025-03-26 20:46:34 +0000

    Commit

---
+++ UF3/EXAMEN/config.php
@@ -0,0 +1,11 @@
+<?php
+
+$host = 'mysql-davidescalonagarcia.alwaysdata.net';
+$dname = 'davidescalonagarcia_examen';
+$username = '393689_examen';
+$password = 'Alumno_1516';
+
+// Crear una nueva conexión MySQLi
+$mysqli = new mysqli($host, $username, $password, $dname);
+
+?>
